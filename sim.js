5e7f787d661bd66d071e19232a1374a228e09999
manidk1239 <117295633+manidk1239@users.noreply.github.com>
2022-11-02 13:41:51 -0400

    Add files via upload

--- sim.js
+++ sim.js
@@ -8,11 +8,11 @@ function simControl() {
 	
             document.getElementById("simLog").style.display = "inline-block";
             document.getElementById("simLog").value = "Starting with " + SpoilerJSON["locations"]["Song from Impa"] + " and " + SpoilerJSON["locations"]["Links Pocket"] + "\n";
-            document.getElementById("simCheckPedestal").style.display = "block";
+            document.getElementById("simCheckAltar").style.display = "block";
             document.getElementById("simCheckChildSpawn").style.display = "block";
             document.getElementById("simCheckAdultSpawn").style.display = "block";
             document.getElementById("gossips").value = "ON";
-	
+            
             if(SpoilerJSON["randomized_settings"]["starting_age"] == undefined) {
                 // if random starting age is not on, assume child vanilla spawn
                 document.getElementById("simLog").value = "Child Spawn: KF Links House\n" + document.getElementById("simLog").value;
